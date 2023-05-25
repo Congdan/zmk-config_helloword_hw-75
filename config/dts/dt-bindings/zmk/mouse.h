5c2241057faede88720a1ae1a070b888aaf5074a
Congdan Huang <congdanhuang@gmail.com>
2023-05-25 15:23:20 +0800

    scroll horizontally

--- config/dts/dt-bindings/zmk/mouse.h
+++ config/dts/dt-bindings/zmk/mouse.h
@@ -2,6 +2,22 @@
  * Copyright (c) 2022-2023 XiNGRZ
  * SPDX-License-Identifier: MIT
  */
+#include <dt-bindings/zmk/mouse.h>
 
-#define MW_UP(n) (0xFF - n)
-#define MW_DN(n) (n)
+#define U_MOUSE_SCROLL_MAX 100
+#define U_MOUSE_SCROLL_DELAY 0
+#define U_MOUSE_SCROLL_EXPONENT 1
+#define U_MOUSE_SCROLL_TIME 0
+
+#undef SCROLL_UP
+#undef SCROLL_DOWN
+#undef SCROLL_LEFT
+#undef SCROLL_RIGHT
+
+#define SCROLL_UP 		SCROLL_VERT(U_MOUSE_SCROLL_MAX)
+#define SCROLL_DOWN 	SCROLL_VERT(-U_MOUSE_SCROLL_MAX)
+#define SCROLL_LEFT 	SCROLL_HOR(-U_MOUSE_SCROLL_MAX)
+#define SCROLL_RIGHT 	SCROLL_HOR(U_MOUSE_SCROLL_MAX)
+
+// #define MW_UP(n) (0xFF - n)
+// #define MW_DN(n) (n)
